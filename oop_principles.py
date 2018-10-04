0bd3d856d0851e203813a92da9b66a793c8e24b0
unknown <kamaumike2013@gmail.com>
2018-10-04 23:33:35 +0300

    Add signup() method

--- oop_principles.py
+++ oop_principles.py
@@ -3,3 +3,10 @@ class User:
     
     def __init__(self):
         pass
+
+    def signup(self,first_name,last_name,email,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        return "Registration was successful"
